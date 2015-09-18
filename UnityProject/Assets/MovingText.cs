38116616ba72e6bdad265c2c30dae9ee941df212
Robin Palmberg <robinpa@kth.se>
2015-09-18 17:52:13 +0200

    Text, Scoreboard, Intro
    Added all the parts on the scoreboard.  Working dynamic text. Hidden
    splash screen and opening crawl.

---
+++ UnityProject/Assets/MovingText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MovingText : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        speed=5f;
+
+        Vector3 movement = new Vector3 (0.0f, 1, 1);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ UnityProject/Assets/ScoreTexter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreTexter : MonoBehaviour {
+
+	private Text ScoreText;
+	public int score;
+
+	// Use this for initialization
+	void Start () {
+	score=0;
+
+	ScoreText = GetComponent<Text>();
+
+	}
+	void Update()
+	{
+		int n=0;
+		while (++n<10)
+		{
+			AddScore(10);
+		}
+	}
+	public void AddScore(int newScoreValue)
+	{
+	score+= newScoreValue;
+	UpdateScore();
+	}
+	// Update is called once per frame
+	void UpdateScore () {
+	ScoreText.text="Score: "+score;
+	}
+}
