76ab9d9733ab2580c9cb1856b7827936511a6634
Stefan Seibert <mail@stefanseibert.com>
2015-10-16 11:48:09 +0200

    Improved Server and Client so they can reconnect while running. Also the communication was changed to Quaternion from Vector3.

--- UnityProject_mobileSaber/Assets/Scripts/RotationDistributor.cs
+++ UnityProject_mobileSaber/Assets/Scripts/RotationDistributor.cs
@@ -1,46 +1,273 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+
+using System;
+using System.Text;
+using System.Threading;
 using System.Collections;
+using System.Net;
 using System.Net.Sockets;
+
+// State object for receiving data from remote device.
+public class StateObject
+{
+    // Client socket.
+    public Socket workSocket = null;
+    // Size of receive buffer.
+    public const int BufferSize = 256;
+    // Receive buffer.
+    public byte[] buffer = new byte[BufferSize];
+    // Received data string.
+    public StringBuilder sb = new StringBuilder();
+
+}
 
 public class RotationDistributor : MonoBehaviour {
 
+    public string standardIP = "192.168.0.14";
 
-    private float speed = 10.0F;
-    private int port = 8888;
-    private GameObject GUI;
-    private Text IPText;
-    System.Net.Sockets.TcpClient clientSocket;
+    //private float speed = 10.0F;
+    private int port = 25005;
+    private InputField IP;
+    private GameObject button;
+    private static bool connected = false;
+    private Socket client;
+    private Transform cubeTransform; 
+    private static Quaternion cubeRotation;
+    private Quaternion quatMult;
 
-    public void Connection()
-    {
-        GUI.SetActive(false);
-        string ip = IPText.text;
+    // ManualResetEvent instances signal completion.
+    private static ManualResetEvent connectDone = new ManualResetEvent(false);
+    private static ManualResetEvent sendDone = new ManualResetEvent(false);
+    private static ManualResetEvent receiveDone = new ManualResetEvent(false);
 
-        Debug.Log("Connecting to Server [" + ip + ":" + port + "]");
-        clientSocket.Connect(ip, port);
-    }
-
-    
     void Start ()
     {
-        clientSocket = new System.Net.Sockets.TcpClient();
-        GUI = GameObject.Find("GUI");
-        IPText = GameObject.Find("IPText").GetComponent<Text>();
-        IPText.text = "127.0.0.1";
-    }
-	
+        connected = false;
+        button = GameObject.Find("Button");
+        cubeTransform = GameObject.Find("Phone").GetComponent<Transform>();
+        IP = GameObject.Find("InputField").GetComponent<InputField>();
+        IP.text = standardIP;
+        Input.gyro.updateInterval = 0.01F;
+        Input.gyro.enabled = true;
 
-	void Update ()
+        #if UNITY_IPHONE
+            quatMult = new Quaternion(0f, 0f, 0.7071f, 0.7071f);
+        #endif
+        #if UNITY_ANDROID
+            quatMult = new Quaternion(0f, 0f, 0.7071f, -0.7071f);
+        #endif
+        #if UNITY_EDITOR
+            quatMult = Quaternion.identity;
+        #endif
+    }
+
+    void Update ()
     {
+        if(connected)
+        {
+            button.GetComponentInChildren<Text>().text = "Disconnect";
+        }
+        else
+        {
+            button.GetComponentInChildren<Text>().text = "Connect";
+        }
+
+        /* Rotation from acceleration first test
         Vector3 dir = Vector3.zero;
         dir.x = -Input.acceleration.y;
         dir.z = Input.acceleration.x;
         if (dir.sqrMagnitude > 1)
             dir.Normalize();
-
         dir *= Time.deltaTime;
-        transform.Translate(dir * speed);
+        Vector3 trans = dir * speed;
+        this.transform.rotation = Quaternion.Euler(trans); */
+        Quaternion rotation;
 
+         #if UNITY_IPHONE
+            rotation = Input.gyro.attitude;
+        #endif
+        #if UNITY_ANDROID
+            rotation = Quaternion(Input.gyro.attitude.w, Input.gyro.attitude.x, Input.gyro.attitude.y, Input.gyro.attitude.z);
+        #endif
+        #if UNITY_EDITOR
+            rotation = Quaternion.identity;
+        #endif
+
+        cubeTransform.localRotation = rotation * quatMult;
+
+        if(connected)
+        {
+            try
+            {
+                Send(client, "<BOF>" + rotation.ToString() + "<EOF>");
+                sendDone.WaitOne();
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e.ToString());
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if(connected)
+        {
+            //client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+    }
+
+    public void Connection()
+    {
+        if(!connected)
+        {
+            string ip = IP.text;
+            Debug.Log("[CLIENT] Connecting to Server [" + ip + ":" + port + "]"); 
+
+            try
+            {
+                IPAddress ipAdress = IPAddress.Parse(ip);
+                IPEndPoint remoteEP = new IPEndPoint(ipAdress, port);
+
+                // Create a TCP/IP socket.
+                client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect to the remote endpoint.
+                client.BeginConnect( remoteEP, new AsyncCallback(ConnectCallback), client);
+                connectDone.WaitOne();
+
+                Receive(client);
+
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e.ToString());
+            }
+        }
+        else
+        {
+            //client.Shutdown(SocketShutdown.Both);
+            client.Close();
+            connected = false;
+        }
+
+    }
+
+     bool SocketConnected(Socket s)
+    {
+        bool part1 = s.Poll(1000, SelectMode.SelectRead);
+        bool part2 = (s.Available == 0);
+        if (part1 && part2)
+            return false;
+        else
+            return true;
+    }
+
+    private static void Receive(Socket client)
+    {
+        try
+        {
+            // Create the state object.
+            StateObject state = new StateObject();
+            state.workSocket = client;
+
+            // Begin receiving the data from the remote device.
+            client.BeginReceive( state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    private static void ReceiveCallback( IAsyncResult ar )
+    {
+        if(!connected)
+        {
+            connected = true;
+            Debug.Log("[CLIENT] Connected to Server successfully.");
+        }
+
+        else
+        {
+            try
+            {
+                // Retrieve the state object and the client socket 
+                // from the asynchronous state object.
+                StateObject state = (StateObject) ar.AsyncState;
+                Socket client = state.workSocket;
+    
+                    // Read data from the remote device.
+                int bytesRead = client.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    // There might be more data, so store the data received so far.
+                    state.sb.Append(Encoding.ASCII.GetString(state.buffer,0,bytesRead));
+    
+                        // Get the rest of the data.
+                        client.BeginReceive(state.buffer,0,StateObject.BufferSize,0,new AsyncCallback(ReceiveCallback), state);
+                }
+                else
+                {
+                    receiveDone.Set();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.ToString());
+            }
+        }
+    }
+
+    private static void Send(Socket client, String data)
+    {
+        // Convert the string data to byte data using ASCII encoding.
+        byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+        // Begin sending the data to the remote device.
+        client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), client);
+    }
+
+    private static void SendCallback(IAsyncResult ar) {
+        try
+        {
+            //Retrieve the socket from the state object.
+            //Socket client = (Socket) ar.AsyncState;
+
+            // Complete sending the data to the remote device.
+            //int bytesSent = client.EndSend(ar);
+            //Debug.Log("Sent " + bytesSent + " bytes to server.");
+
+            // Signal that all bytes have been sent.
+            sendDone.Set();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    private static void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the socket from the state object.
+            Socket client = (Socket) ar.AsyncState;
+
+            // Complete the connection.
+            client.EndConnect(ar);
+
+            Debug.Log("[CLIENT] Socket connected to " + client.RemoteEndPoint.ToString());
+
+            // Signal that the connection has been made.
+            connectDone.Set();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
     }
 }
---
+++ UnityProject_mobileSaber/Assets/Scripts/RotationServer.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+using UnityEngine;
+
+public class RotationServer : MonoBehaviour
+{
+    public string ip = "192.168.0.14";
+    private int port = 25005;
+    private Transform cubeTransform; 
+    private static Quaternion cubeRotation;
+    private Socket listener;
+    private static RotationServer server;
+
+    void Start()
+    {
+        cubeTransform = GameObject.Find("Phone").GetComponent<Transform>();
+        cubeRotation = Quaternion.identity;
+        StartListening();
+        server = this;
+    }
+
+    void Update()
+    {
+        cubeTransform.localRotation = cubeRotation;
+    }
+
+    // Thread signal.
+    public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+    public void StartListening()
+    {
+
+        IPAddress ipAdress = IPAddress.Parse(ip);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAdress, port);
+
+        // Create a TCP/IP socket.
+        listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
+
+        // Bind the socket to the local endpoint and listen for incoming connections.
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(2);
+
+                // Start an asynchronous socket to listen for connections.
+                Debug.Log("[SERVER] Waiting for a connection...");
+                listener.BeginAccept(new AsyncCallback(AcceptCallback), listener );
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    public static void AcceptCallback(IAsyncResult ar)
+    {
+        // Signal the main thread to continue.
+        allDone.Set();
+
+        // Get the socket that handles the client request.
+        Socket listener = (Socket) ar.AsyncState;
+        Socket handler = listener.EndAccept(ar);
+
+        // Create the state object.
+        StateObject state = new StateObject();
+        state.workSocket = handler;
+        handler.BeginReceive( state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+        Send(handler, "Hello!");
+
+    }
+
+    public static void ReadCallback(IAsyncResult ar)
+    {
+        String content = String.Empty;
+
+        // Retrieve the state object and the handler socket
+        // from the asynchronous state object.
+        StateObject state = (StateObject) ar.AsyncState;
+        Socket handler = state.workSocket;
+
+        try
+        {
+            // Read data from the client socket. 
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer,0,bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read 
+                // more data.
+                content = state.sb.ToString();
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    // One block of data has been read from the client. Display it on the console.
+                    //Debug.Log("[SERVER] Read " + content.Length  + " bytes from socket. Data : " + content + "\n");
+                    getQuaternionFromString(content);
+                    bytesRead = 0;
+                    state.sb.Length = 0;
+                }
+
+                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+            }
+            // we received Zero, client disconnected
+            else if(bytesRead == 0)
+            {
+                server.closeConnection();
+            }
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.ToString());
+            server.closeConnection();
+        }
+    }
+
+    private void closeConnection()
+    {
+        if(listener != null)
+        {
+            listener.Close();
+            listener = null;
+        }
+        StartListening();
+    }
+
+    private static bool getQuaternionFromString(string str)
+    {
+        string[] temp1 = str.Split('>');
+        string[] temp2 = temp1[1].Split('<');
+
+        string vector = temp2[0];
+
+        if(vector.StartsWith("(") && vector.EndsWith(")"))
+        {
+            string[] temp = vector.Substring(1,vector.Length-2).Split(',');
+            float x = float.Parse(temp[0]);
+            float y = float.Parse(temp[1]);
+            float z = float.Parse(temp[2]);
+            float w = float.Parse(temp[3]);
+            Quaternion inRot = new Quaternion(x,y,z,w);
+            //Vector3 euRot = inRot.eulerAngles;
+            //cubeRotation = Quaternion.Euler(euRot.x, euRot.z, euRot.y);
+            cubeRotation = inRot;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    private static void Send(Socket handler, String data)
+    {
+        // Convert the string data to byte data using ASCII encoding.
+        byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+        // Begin sending the data to the remote device.
+        handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+    }
+
+    private static void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the socket from the state object.
+            //Socket handler = (Socket) ar.AsyncState;
+
+            // Complete sending the data to the remote device.
+            //int bytesSent = handler.EndSend(ar);
+            //Debug.Log("Sent " + bytesSent + " bytes to client.");
+            Debug.Log("[SERVER] Sent Initialization Sequence to Client.");
+        }
+        catch (Exception e) 
+        {
+            Debug.Log(e.ToString());
+            server.closeConnection();
+        }
+    }
+}
